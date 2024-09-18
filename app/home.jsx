d72a71fabb93e94af29f2f7f4242f787ac8689f0
Sabaragamuwa S.A.T.D-IT21836268 <it21836268@my.sliit.lk>
2024-09-18 22:55:01 +0530

    image slider with some error

--- app/home.jsx
+++ app/home.jsx
@@ -1,19 +1,21 @@
-import { View, Text, SafeAreaView, StatusBar } from "react-native";
+import { View, Text, SafeAreaView, StatusBar, Image } from "react-native";
 import React from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import ImageSlider from "../components/imageSlider.jsx";
 
 export default function Home() {
   return (
-    <SafeAreaView className="flex-1 bg-white flex space-y-5" edges={["top"]}>
+    <SafeAreaView className="flex flex-1 space-y-5 bg-white" edges={["top"]}>
       <StatusBar style={"dark"} />
       {/* //Note: Punchline and Avatar....................................................... */}
-      <View className="flex-row justify-between items-center mx-5">
+      <View className="flex-row items-center justify-between mx-5">
         <View className="space-y-2">
           <Text
-            className="font-bold tracking-wider text-neutral-700"
+            className="mt-2 font-bold tracking-wider text-neutral-700"
             style={{ fontSize: hp(4.5) }}
           >
             READY TO
@@ -25,6 +27,23 @@ export default function Home() {
             WORKOUT
           </Text>
         </View>
+        <View className="flex items-center justify-center space-y-2">
+          <Image
+            source={require("../assets/images/profile.jpeg")}
+            style={{ height: hp(6), width: hp(6) }}
+            className="rounded-full"
+          />
+          <View
+            className="flex items-center justify-center border-[3px] rounded-full bg-neutral-200 border-neutral-200"
+            style={{ height: hp(5.5), width: hp(5.5) }}
+          >
+            <Ionicons name="notifications" size={30} color="#000000" />
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <ImageSlider />
       </View>
     </SafeAreaView>
   );
